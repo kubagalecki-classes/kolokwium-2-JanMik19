da9760f968f876a1f8e8f5f175358255d8ee8868
JanMik19 <44546286+JanMik19@users.noreply.github.com>
2021-01-25 18:10:40 +0100

    Update zad1.hpp
    update2

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -12,10 +12,16 @@ class Warzywo
     std::string            nazwa;
     double                 cena;
     int                    kolor;
-    inline static unsigned liczba = 0;
+    inline static unsigned liczba;
 
 public:
-    Warzywo(const std::string& n, double c, int k) : nazwa(n), cena(c), kolor(k) { liczba++; }
+    Warzywo(const std::string& n, double c, int k)
+    {
+        nazwa = n;
+        cena  = c;
+        kolor = k;
+        liczba++;
+    }
     ~Warzywo() { liczba--; }
     static unsigned getVeg() { return liczba; }
     void            opis(std::ostream& cout) const
@@ -24,5 +30,3 @@ public:
              << "[" << kolor << "]" << std::endl;
     }
 };
-
-// inline unsigned Warzywo::liczba = 0;
