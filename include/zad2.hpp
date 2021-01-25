0e33123646d218c2d130a1ba6a70ff9d020d3a45
JanMik19 <44546286+JanMik19@users.noreply.github.com>
2021-01-25 18:31:35 +0100

    Update zad2.hpp
    zad 2 update

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -4,3 +4,31 @@
 #include "chrup.hpp"
 
 // tutaj klasy Ogorek, Zielony i Kiszony
+class Ogorek
+{
+public: 
+	virtual std::string chrup() = 0;
+};
+
+class Zielony : public Ogorek
+{
+public:
+    std::string chrup() override { return chrupZielony(); }
+};
+
+class Kiszony : public Ogorek
+{
+public:
+    std::string chrup() override { return chrupKiszony(); }
+};
+
+std::string jedzOgorek(Ogorek* ogorek)
+{
+    std::string text1 = "Zielony";
+    std::string text2 = "Kiszony";
+    
+    if (ogorek == dynamic_cast<Zielony*>(ogorek))
+        return text1 + ogorek->chrup();
+    else
+        return text2 + ogorek->chrup();
+}
