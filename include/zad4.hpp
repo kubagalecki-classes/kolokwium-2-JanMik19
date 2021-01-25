32a01448a51040742752e2ee0efe065e248f0f66
JanMik19 <44546286+JanMik19@users.noreply.github.com>
2021-01-25 19:03:22 +0100

    Update zad4.hpp
    zad4

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -5,3 +5,17 @@
 #include <vector>
 
 // tutaj funkcja jedzOstatnieWarzywa
+void jedzOstatnieWarzywa(const std::vector<Warzywo>& vec, unsigned n, std::ostream& cout)
+{
+    if (n >= vec.size())
+        throw std::logic_error "N wieksze niz wektor!";
+    else
+    {
+        int x = vec.size() - n - 1;
+
+        for (int i = vec.size() - 2; i >= x; i--)
+        {
+            vec[i].opis(cout);
+        }
+    }
+}
