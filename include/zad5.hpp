53a84b1ab1422be291ce96755c6f130b8eb5b153
JanMik19 <44546286+JanMik19@users.noreply.github.com>
2021-01-25 19:19:03 +0100

    Update zad5.hpp
    zad5

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -5,3 +5,9 @@
 #include <vector>
 
 // tutaj algorytm liczKolejneWarzywa
+template<typename T>
+bool liczKolejneWarzywa(T iter1, T iter2, unsigned n, const std::string& s)
+{
+    Warzywo warz;
+    if (search_n(iter1, iter2, n, warz, s) == poczatek) ? true : false;
+}
