34ed2630837df9dad63da262afad038579e33c0c
JanMik19 <44546286+JanMik19@users.noreply.github.com>
2021-01-25 19:20:41 +0100

    Update zad5.hpp
    do poprawy

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -9,5 +9,5 @@ template<typename T>
 bool liczKolejneWarzywa(T iter1, T iter2, unsigned n, const std::string& s)
 {
     Warzywo warz;
-    if (search_n(iter1, iter2, n, warz, s) == poczatek) ? true : false;
+    //if (search_n(iter1, iter2, n, warz, s) == poczatek) ? true : false;
 }
